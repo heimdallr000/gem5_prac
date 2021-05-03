50ae6654986e88fa6789352a08b9a2478031e9ab
heimdallr000 <phink608@gmail.com>
2021-05-03 23:08:58 +0900

    .

--- disk_image/runscript.sh
+++ disk_image/runscript.sh
@@ -8,29 +8,19 @@ if [ -s workloads ]; then
     # if the file is not empty, run spec with the parameters
     echo "Workload detected"
     echo "Reset stats"
-    free
     m5 exit
-    echo "checkpoint 1"
 
     # run the commands
-    echo "checkpoint 2"
     read -r workload size m5filespath < workloads
-    echo "checkpoint 3"
     runcpu --size $size --iterations 1 --config myconfig.x86.cfg --noreportable --nobuild $workload
-    echo "checkpoint 4"
     m5 exit
-    echo "checkpoint 5"
     
     # copy the SPEC result files to host
-    echo "checkpoint 6"
     for filepath in /home/gem5/spec2017/result/*; do
         filename=$(basename $filepath)
-        echo "checkpoint 7"
         m5 writefile $filepath $m5filespath/$filename
     done
-    echo "checkpoint 8"
     m5 exit
-    echo "checkpoint 9"
 else
     echo "Couldn't find any workload"
     m5 exit
